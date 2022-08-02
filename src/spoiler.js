ad1afa453f44b4d9c9778befebcdfe566d7237b5
rogier-stegeman <rogier.stegeman@vartion.com>
2022-08-02 23:44:51 +0200

    feat(spoiler): broader Google search href matching

--- src/spoiler.js
+++ src/spoiler.js
@@ -83,7 +83,7 @@ function youtube() {
 function main() {
     let hostFound = true;
     const href = window.location.href;
-    if (href.startsWith('https://www.google.com/search') || href.startsWith('https://www.google.com/webhp')) {
+    if (href.match(/^https:\/\/www\.google\.[a-z]+\/($|search|webhp|imghp)/)) {
         google();
     }
     else if (href.startsWith('https://www.youtube.com/watch')) {
